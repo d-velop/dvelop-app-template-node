bb3d5ee8aa465c58d31c71bf155a7f74e641f232
Lennart Klose <lennart.klose@d-velop.de>
2022-10-28 16:58:19 +0200

    Remove warnings for old syntax

--- terraform/modules/cloudfront_distribution/main.tf
+++ terraform/modules/cloudfront_distribution/main.tf
@@ -8,17 +8,17 @@ provider "aws" {
 
 # cf. https://www.terraform.io/docs/providers/aws/d/route53_zone.html
 data "aws_route53_zone" "hosted_zone" {
-  zone_id = "${var.hosted_zone_id}"
+  zone_id = var.hosted_zone_id
 }
 
 locals {
   // cf. https://github.com/terraform-providers/terraform-provider-aws/issues/241#issuecomment-438744460
-  hosted_zone_name    = "${replace(data.aws_route53_zone.hosted_zone.name, "/[.]$/", "")}"
+  hosted_zone_name    = replace(data.aws_route53_zone.hosted_zone.name, "/[.]$/", "")
 }
 
 # cf. https://www.terraform.io/docs/providers/aws/r/acm_certificate.html
 resource "aws_acm_certificate" "cert" {
-  domain_name               = "${local.hosted_zone_name}"
+  domain_name               = local.hosted_zone_name
   subject_alternative_names = ["*.${local.hosted_zone_name}"]
   validation_method         = "DNS"
   provider                  = "aws.virginia"
@@ -30,11 +30,11 @@ resource "aws_acm_certificate" "cert" {
 
 # cf. https://www.terraform.io/docs/providers/aws/r/acm_certificate_validation.html
 resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn = "${aws_acm_certificate.cert.arn}"
+  certificate_arn = aws_acm_certificate.cert.arn
 
   validation_record_fqdns = [
-    "${aws_route53_record.cert_name_validation.fqdn}",
-    "${aws_route53_record.cert_alt_name_validation.fqdn}",
+    aws_route53_record.cert_name_validation.fqdn,
+    aws_route53_record.cert_alt_name_validation.fqdn
   ]
 
   provider = "aws.virginia"
@@ -43,28 +43,28 @@ resource "aws_acm_certificate_validation" "cert" {
 # cf. https://www.terraform.io/docs/providers/aws/r/acm_certificate_validation.html
 resource "aws_route53_record" "cert_name_validation" {
   allow_overwrite = true
-  name    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.domain_validation_options.0.resource_record_type}"
-  zone_id = "${var.hosted_zone_id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.0.resource_record_value}"]
+  name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
+  zone_id = var.hosted_zone_id
+  records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
   ttl     = 60
 }
 
 # cf. https://www.terraform.io/docs/providers/aws/r/acm_certificate_validation.html
 resource "aws_route53_record" "cert_alt_name_validation" {
   allow_overwrite = true
-  name    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_name}"
-  type    = "${aws_acm_certificate.cert.domain_validation_options.1.resource_record_type}"
-  zone_id = "${var.hosted_zone_id}"
-  records = ["${aws_acm_certificate.cert.domain_validation_options.1.resource_record_value}"]
+  name    = aws_acm_certificate.cert.domain_validation_options.1.resource_record_name
+  type    = aws_acm_certificate.cert.domain_validation_options.1.resource_record_type
+  zone_id = var.hosted_zone_id
+  records = [aws_acm_certificate.cert.domain_validation_options.1.resource_record_value]
   ttl     = 60
 }
 
 # cf. https://www.terraform.io/docs/providers/aws/r/cloudfront_distribution.html
 resource "aws_cloudfront_distribution" "dist" {
   origin {
-    origin_id   = "${sha256(var.origin_domain_name)}"
-    domain_name = "${var.origin_domain_name}"
+    origin_id   = sha256(var.origin_domain_name)
+    domain_name = var.origin_domain_name
   }
 
   enabled         = true
@@ -78,7 +78,7 @@ resource "aws_cloudfront_distribution" "dist" {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
     compress         = true
-    target_origin_id = "${sha256(var.origin_domain_name)}"
+    target_origin_id = sha256(var.origin_domain_name)
 
     forwarded_values {
       query_string = true
@@ -120,13 +120,13 @@ resource "aws_cloudfront_distribution" "dist" {
 }
 
 resource "aws_route53_record" "dist" {
-  zone_id = "${var.hosted_zone_id}"
+  zone_id = var.hosted_zone_id
   name    = "${var.custom_subdomain_name}.${local.hosted_zone_name}"
   type    = "A"
 
   alias {
-    name                   = "${aws_cloudfront_distribution.dist.domain_name}"
-    zone_id                = "${aws_cloudfront_distribution.dist.hosted_zone_id}"
+    name                   = aws_cloudfront_distribution.dist.domain_name
+    zone_id                = aws_cloudfront_distribution.dist.hosted_zone_id
     evaluate_target_health = false
   }
 }
