356c952f5a599e336eca6a543a7dcb1e8b17d296
Lennart Klose <lennart.klose@d-velop.de>
2022-10-31 14:11:51 +0100

    Remove unneeded providers

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -7,22 +7,9 @@ terraform {
       source = "hashicorp/aws",
       version = "4.37.0"
     }
-
-    template = {
-      source = "hashicorp/template"
-      version = "2.1.2"
-    }
-
-    null = {
-      source = "hashicorp/null"
-      version = "2.1.2"
-    }
   }
 }
 
 provider "aws" {
   region = "eu-central-1"
 }
-
-provider "template" { }
-provider "null" { }
